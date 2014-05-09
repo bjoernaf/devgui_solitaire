477c2fde93ebbb155bc9aac60d084a0697b2c590
lundgren87 <lundgren87@gmail.com>
2014-05-09 18:39:03 +0200

    Minor fix in Slider.

--- code/view/transSlider.py
+++ code/view/transSlider.py
@@ -18,8 +18,12 @@ class transSlider(QSlider):
         '''
         super(transSlider, self).__init__(Qt.Horizontal)
         self.gsc = gsc
-        self.setSliderPosition(self.gsc.opacity)
         self.setMaximumWidth(200)
+        
+        # Set minimum and maximum value of the slider, then get value from gsc
+        self.setMinimum(0)
+        self.setMaximum(100)
+        self.setSliderPosition(self.gsc.opacity)
         
         # Set up and send signal to update tooltip
         self.valueChanged.connect(self.updateToolTip)
@@ -29,7 +33,7 @@ class transSlider(QSlider):
         '''
         Updates the tooltip to display the current value
         '''
-        self.toolTipString = "Card Opacity: " + str(self.value())
+        self.toolTipString = "Card Opacity: " + str(self.value()) + "%"
         self.setToolTip(self.toolTipString)
     
     def mouseMoveEvent(self, event):
@@ -38,17 +42,18 @@ class transSlider(QSlider):
         '''
         super(transSlider, self).mouseMoveEvent(event)
         self.gsc.opacity = self.sliderPosition()
+        self.gsc.solWin.bView.scene.update()
     
-    def mousePressEvent(self, event):
+    def mouseReleaseEvent(self, event):
         '''
-        Override of mousePressEvent that moves the slider to the
+        Override of mouseReleaseEvent that moves the slider to the
         position pressed on the scale, and sets the opacity in
         gameStateController accordingly.
         '''
-        self.gsc.solWin.bView.scene.update()
         posClicked = self.minimum() + ((self.maximum()-self.minimum()) * event.x()) / self.width()
         if (self.sliderPosition() != posClicked):
             self.setValue(posClicked)
             self.gsc.opacity = self.sliderPosition()
-        QSlider.mousePressEvent(self, event)
+            self.gsc.solWin.bView.scene.update()
+        QSlider.mouseReleaseEvent(self, event)
         # http://stackoverflow.com/questions/11132597/qslider-mouse-direct-jump
